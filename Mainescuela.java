e30adcc0f76324cb2e72a112ee480c4aed3bb44b
Sputnikjoa <joaquin.ros.sed15@gmail.com>
2021-11-11 10:59:44 -0600

    commit

---
+++ Mainescuela.java
@@ -0,0 +1,11 @@
+
+package mainescuela;
+
+public class Mainescuela {
+
+    public static void main(String[] args) {
+        Escuela escuela1=new Escuela(1,"ESCOM");
+        System.out.println(escuela1.getNombre());
+    }
+    
+}
