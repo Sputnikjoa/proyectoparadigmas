6aa109ce62419ca2b1936f1ae1b60f2f17d4a7ab
Sputnikjoa <joaquin.ros.sed15@gmail.com>
2021-11-11 10:40:37 -0600

    commit main

--- mainescuela.java
+++ mainescuela.java
@@ -3,6 +3,9 @@ package mainescuela;
 import mainescuela.escuela;
 
 public class mainescuela {
-    Escuela escuela1= new Escuela(1,"ESCOM");
-    System.out.println(escuela1.getEscuela());
+    public static void main(String[] args) {
+        Escuela escuela1= new Escuela(1,"ESCOM");
+        System.out.println(escuela1.getEscuela()); 
+    }
+    
 }
